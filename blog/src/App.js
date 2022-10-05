bf5f089ee3b9f2bedc3403eb0814c701f1fae1fe
DaeWon9 <qkreodnjs97@naver.com>
2022-10-05 17:13:45 +0900

    [ADD] data binging test

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '바인딩 테스트'; 
+
+  function test(){
+    return 100;
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <img src={ logo }></img>
+      <h4>{ posts }</h4>
     </div>
   );
 }
