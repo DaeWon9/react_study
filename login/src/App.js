1c9d32ce7e39914e2edb87ca3e199addde60b6aa
DaeWon9 <qkreodnjs97@naver.com>
2022-10-08 15:36:57 +0900

    [ADD] router

--- login/src/App.js
+++ login/src/App.js
@@ -1,42 +1,16 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Login from './components/Login';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
-        </div>
-      </form>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Login />
+      </div>
+    </BrowserRouter>
   );
 }
 
